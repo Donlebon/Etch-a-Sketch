c6239e88342e8f2d41dc31f7ff15ebdbe6d9e0ae
Don <donducdo@gmail.com>
2022-07-06 23:12:24 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let container = document.querySelector(".frame-container");
+
+for(let i = 0; i < 100; i++){
+    let frames = document.createElement("div");
+    frames.classList.add("frames");
+    container.appendChild(frames)
+}
+
+
+
